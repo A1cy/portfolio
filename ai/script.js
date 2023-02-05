f1331f3cf248083aba6859013a07217969689b5a
A1cy <a1hvdy@gmail.com>
2023-02-05 11:18:28 +0300

    new server

--- ai/script.js
+++ ai/script.js
@@ -101,7 +101,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://aihadi.onrender.com', {
+    const response = await fetch('https://mmate.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
